eb36454fcb584dc86307f702743d78421e98c655
Etchegoyen Matthieu <etchegom@gmail.com>
2024-05-06 10:34:35 +0200

    WIP

--- recoco/apps/metrics/sql_queries/mec_stats.py
+++ recoco/apps/metrics/sql_queries/mec_stats.py
@@ -1,36 +1,16 @@
 from recoco.apps.projects.models import Project, ProjectMember
-from recoco.apps.addressbook.models import Organization
 from datetime import timedelta, date
 from django.db.models import Count, DecimalField, Q, ExpressionWrapper
-from recoco.apps.home.models import UserProfile
+from recoco.apps.tasks.models import Task
+from recoco.apps.projects.models import ProjectSwitchtender
 
 mec_site_id = 6
 
-# mec_site = Site.objects.get(pk=mec_site_id)
 
-
-def list_organizations():
-    organizations = Organization.objects.filter(sites__pk=mec_site_id)
-    for org in organizations:
-        print(org.name)
-
-
-def list_users():
-    mec_users = UserProfile.objects.filter(
-        sites__pk=mec_site_id, organization__isnull=False
-    )
-    # for user in mec_users:
-    #     print(
-    #         f"{user.user.username}, {user.organization.name}, {user.organization_position}"
-    #     )
-
-    qs = mec_users.aggregate(count_org=Count("organization"))
-    print(qs)
-
-
-def cdp_metrics(nb_days: int = 7):
+def mec_metrics(nb_days: int = 7):
 
     for nb_days in [7, 14, 30]:
+        print(f"Ces {nb_days} derniers jours:")
         last_n_days = date.today() - timedelta(days=nb_days)
 
         # 1. CDP connectés au moins 1 fois
@@ -39,26 +19,32 @@ def cdp_metrics(nb_days: int = 7):
             project__sites__pk=mec_site_id,
             member__last_login__date__gte=last_n_days,
         )
-        print(f"Nombre de CDP connectés ces {nb_days} derniers jours: {qs.count()}")
+        print(f"\t- Nombre de CDP connectés: {qs.count()}")
 
         # 2. Collectivités connectés au moins 1 fois
         qs = Project.objects.filter(sites__pk=mec_site_id).filter(
             last_members_activity_at__date__gte=last_n_days
         )
-        print(
-            f"Nombre de collectivités connectés ces {nb_days} derniers jours: {qs.count()}"
-        )
+        print(f"\t- Nombre de collectivités connectées: {qs.count()}")
 
         # 3. Conseillers connectés au moins 1 fois
-        # TODO
+        qs = ProjectSwitchtender.objects.filter(site_id=mec_site_id).filter(
+            switchtender__last_login__date__gte=last_n_days
+        )
+        print(f"\t- Nombre de conseillers connectés: {qs.count()}")
 
-    # 4. Nombre de projets déposés (30 derniers jours + cumulés)
-    # TODO
+        # 4. Nombre de projets déposés (30 derniers jours + cumulés)
+        qs = Project.objects.filter(sites__pk=mec_site_id).filter(
+            created_on__date__gte=last_n_days
+        )
+        print(f"\t- Nombre de projets déposés: {qs.count()}")
 
-    # 5. Nombre de recommandations
-    # TODO
+    # 5. Nombre de recommandations par statut
+    qs = Task.objects.filter(site__pk=mec_site_id)
+    print(f"\t- Nombre de recommandations: {qs.count()}")
 
     # 6. % / statut
+    print("Pourcentage de projets par statut:")
     agg = Project.objects.filter(sites__pk=mec_site_id).aggregate(
         **{
             f"{status.lower()}": ExpressionWrapper(
@@ -69,11 +55,11 @@ def cdp_metrics(nb_days: int = 7):
         }
     )
     for k, v in agg.items():
-        print(f"Pourcentage de projets en statut {k.upper()}: {v}%")
+        print(f"\t- Statut {k.upper()}: {v}%")
 
     # 7. Nombre d’interactions entre collectivités x conseillers (30 derniers jours + cumulés)
     #    (interaction = 1 conversation + 1 recommandation)
     # TODO
 
 
-cdp_metrics()
+mec_metrics()
